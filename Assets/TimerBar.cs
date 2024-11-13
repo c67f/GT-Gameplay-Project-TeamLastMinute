21e20836bda9a898a82e5110b447c92406df4f4e
Cal Friedman School <cffriedm@ucsc.edu>
2024-11-12 22:46:35 -0800

    Initial Timer/Health bar

---
+++ Assets/TimerBar.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerBar : MonoBehaviour
+{
+    public Slider timerBar;
+    public float startHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerBar.maxValue = startHealth;
+        timerBar.minValue = 0;
+        timerBar.value = timerBar.maxValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerBar.value = timerBar.value - Time.deltaTime;
+        if (timerBar.value <= 0)
+        {
+            Debug.Log("Game over!");
+        }
+    }
+}
